4050010c3ecb441a5a747af03c30b4ddeb1e725e
DarielOWL <darieldesosa@gmail.com>
2022-04-01 22:47:07 -0300

    Controller aceptan cualquier IP

--- src/main/java/co/com/sofka/back/Controller/ClienteController.java
+++ src/main/java/co/com/sofka/back/Controller/ClienteController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-@CrossOrigin(origins = "http://localhost:8080")
+@CrossOrigin(origins = "*")
 @RestController
 public class ClienteController {
 
--- src/main/java/co/com/sofka/back/Controller/ProductoController.java
+++ src/main/java/co/com/sofka/back/Controller/ProductoController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-@CrossOrigin(origins = "http://localhost:8080")
+@CrossOrigin(origins = "*")
 @RestController
 public class ProductoController {
 
